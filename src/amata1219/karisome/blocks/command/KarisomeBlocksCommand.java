f736a8bd5ceeded6f6c3bb69c1cdae54faa52825
amata1219 <suzukiamata@gmail.com>
2022-04-20 15:35:57 +0900

    add main command

---
+++ src/amata1219/karisome/blocks/command/KarisomeBlocksCommand.java
@@ -0,0 +1,38 @@
+package amata1219.karisome.blocks.command;
+
+import static org.bukkit.ChatColor.*;
+
+import amata1219.karisome.blocks.KarisomeBlockItem;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class KarisomeBlocksCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("%sThis command can only be executed by the player.".formatted(RED));
+            return true;
+        }
+
+        int amount;
+        if (args.length == 0) amount = 1;
+        else {
+            try {
+                amount = Integer.parseInt(args[0]);
+            } catch(NumberFormatException ex) {
+                player.sendMessage("%sThe amount must be numeric.".formatted(RED));
+                return true;
+            }
+        }
+
+        player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0f, 1.5f);
+
+        player.getInventory().addItem(KarisomeBlockItem.createKarisomeBlocks(amount, 20 * 3));
+
+        player.sendMessage("%sYou have been given &d karisome blocks!".formatted(AQUA, amount));
+        return true;
+    }
+}
